43ef39884b765b738f9a87f6ed2e81164e05aff6
the-robot <the-robot@users.noreply.github.com>
2018-02-17 04:04:24 +0800

    Header, Login Popup

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -1,12 +1,65 @@
-import { Layout } from 'antd';
+import { Button, Input, Layout, Popover } from 'antd';
 import React from 'react';
 
 export default class AppHeader extends React.Component {
+  state = {
+    visible: false,
+  }
+
+  hide = () => {
+    this.setState({
+      visible: false,
+    });
+  }
+
+  handleVisibleChange = (visible) => {
+    this.setState({ visible });
+  }
+  
   render() {
     const { Header } = Layout;
 
+    const headerStyle = {
+      background: 'transparent',
+      padding: 0,
+      paddingRight: '20px',
+      textAlign: 'right',
+    }
+
+    const inputStyle = {
+      width: '180px',
+      display: 'block',
+      marginBottom: '5px',
+    }
+
+    const popOverButton = {
+      display: 'block',
+      margin: '5px',
+    }
+
     return (
-      <Header style={{ background: '#fff', padding: 0 }} />
+      <Header style={ headerStyle } >
+        <Popover
+          content={
+            <div>
+              <Input placeholder='username' style={ inputStyle } />
+              <Input placeholder='password' style={ inputStyle } type='password' />
+
+              <div style={{ marginTop: '15px' }}>
+                <a onClick={this.hide} style={ popOverButton }>Login</a>
+                <a onClick={this.hide} style={ popOverButton }>Register</a>
+              </div>
+            </div>
+          }
+
+          title='Login User'
+          trigger='click'
+          visible={ this.state.visible }
+          onVisibleChange={ this.handleVisibleChange }
+        >
+          <Button type="primary" icon="login">LOGIN</Button>
+        </Popover>
+      </Header>
     );
   }
 }
