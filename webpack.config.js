2b5079659bd7363002258f2aa1cd6d57ea0764e9
the-robot <the-robot@users.noreply.github.com>
2018-02-22 14:44:43 +0800

    webpack / webpack-dev-server updated

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,14 @@ var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 var path = require('path');
 
-module.exports = {
+const config = {
   context: path.join(__dirname, "src"),
   devtool: debug ? "inline-sourcemap" : null,
   entry: "./js/app.jsx",
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "app.min.js"
+  },
   module: {
     loaders: [
       {
@@ -19,19 +23,17 @@ module.exports = {
       }
     ]
   },
-  resolve: {
-    extensions: ['', '.js', '.jsx'],
-  },
-  devServer: {
-    historyApiFallback: true
-  },
-  output: {
-    path: __dirname + "/src/",
-    filename: "app.min.js"
-  },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
   ],
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  devServer: {
+    historyApiFallback: true
+  },
 };
+
+module.exports = config;
