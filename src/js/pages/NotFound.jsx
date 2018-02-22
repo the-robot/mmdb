5f007a414490d32e2e11eab15de92ee8b37c880e
the-robot <the-robot@users.noreply.github.com>
2018-02-22 14:46:11 +0800

    404 page added

---
+++ src/js/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class NotFound extends React.Component {
+  render() {
+    console.log(this.props);
+
+    return (
+      <div>
+        <h1>404</h1>
+      </div>
+    );
+  }
+}
