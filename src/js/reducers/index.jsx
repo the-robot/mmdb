305c0a39f75a3e7031f9c49856dd4744822ab8f9
the-robot <the-robot@users.noreply.github.com>
2018-02-24 22:17:56 +0800

    movie information details reducer created

--- src/js/reducers/index.jsx
+++ src/js/reducers/index.jsx
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 
 import calendar from './calendarReducer';
+import movie from './movieReducer';
 import movies from './moviesReducer';
 import user from './userReducer';
 
 export default combineReducers({
   calendar,
+  movie,
   movies,
   user,
 });
