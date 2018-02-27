3b6fa68a1cc16dbe7cf9ced5e2dbe247d9c40155
the-robot <the-robot@users.noreply.github.com>
2018-02-27 19:25:50 +0800

    Cast react component created

--- src/js/pages/Movie/Cast.jsx
+++ src/js/pages/Movie/Cast.jsx
@@ -1,23 +1,56 @@
-
-import { Row } from 'antd';
+import { Row, List } from 'antd';
+import InfiniteScroll from 'react-infinite-scroller';
 import React from 'react';
 
 import CastCard from '../../components/Movie/CastCard';
+import { getCast } from '../../actions/movie/castAction';
 
 export default class Cast extends React.Component {
-  render() {
-    const cast = this.props.cast;
+  divideCasts(array) {
+    // divide the casts informations into two dimentional array
+    // with 6 casts per each array
+    var data = [];
 
-    const castCards = cast.slice(0, 6).map(
-      (person) => <CastCard key={ person.cast_id } 
-                            name={ person.name }
-                            character={ person.character }
-                            picture={ person.profile_path } />)
+    for (let i=0; i<array.length; i+=6) {
+      data.push(array.slice(i, i+6));
+    }
+
+    return data;
+  }
+
+  getCastCards(cast) {
+    // convert the cast data into CastCard react component
+    var castCards = [];
+
+    for (let i=0; i<cast.length; i++) {
+      let temp = cast[i].map(
+        (person) => <CastCard key={ person.cast_id } 
+                              name={ person.name }
+                              character={ person.character }
+                              picture={ person.profile_path } />);
+      
+      castCards.push(temp);
+    }
+
+    return castCards;
+  }
+
+  render() {
+    const cast = this.divideCasts(this.props.cast);
+    const castCards = this.getCastCards(cast);
 
     return (
-      <Row type="flex" justify="center" gutter={24}>
-        { castCards }
-      </Row>
+      <List
+        itemLayout="vertical"
+        dataSource={ castCards }
+        renderItem={item => (
+          <List.Item>
+            <Row type="flex" justify="center" gutter={24}>
+              { item }
+            </Row>
+          </List.Item>
+        )} 
+      />
     );
   }
 }
