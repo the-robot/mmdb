af42a09f0377f5806ee32f2b1896697bffb5ed3c
the-robot <the-robot@users.noreply.github.com>
2018-03-03 02:58:28 +0800

    implemented getLanguages method

--- src/js/actions/movie/movieAction.jsx
+++ src/js/actions/movie/movieAction.jsx
@@ -35,10 +35,10 @@ function clean(data) {
   var result = {
     id: data.id,
     title: data.title,
-    release_date: dateFormat(data.release_date),
+    release_date: getDate(data.release_date),
     status: data.status,
-    language: data.spoken_languages[0]['name'],
-    runtime: timeFormat(data.runtime),
+    language: getLanguages(data.spoken_languages),
+    runtime: getTime(data.runtime),
     pg: (data.adult ? 'PG18' : 'PG13'),
     homepage: data.homepage,
     summary: data.overview,
@@ -50,20 +50,30 @@ function clean(data) {
   return result;
 }
 
-function percentage(number, max) {
-  return Math.ceil((number * 100) / max);
+function getLanguages(languages) {
+  var langs = '';
+
+  // remove empty items
+  for (let lang of languages) {
+    if (lang['name'].length === 0)
+      languages.splice(languages.indexOf(lang), 1);
+  }
+
+  // convert into string
+  for (let lang of languages) {
+    // if last element, do not add comma
+    if (languages.indexOf(lang) === languages.length - 1) {
+      langs += lang['name'];
+      break;
+    }
+
+    langs += lang['name'] + ', ';
+  }
+
+  return langs;
 }
 
-function getGenres(data) {
-  var genres = [];
-
-  for (let i=0; i<data.length; i++)
-    genres.push(data[i]['name']);
-
-  return genres;
-}
-
-function dateFormat(date) {
+function getDate(date) {
   const months = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
   ];
@@ -77,7 +87,7 @@ function dateFormat(date) {
   return month + " " + day + " " + year;
 }
 
-function timeFormat(minute) {
+function getTime(minute) {
   var hour = Math.floor(minute / 60);
   var min = minute % 60;
 
@@ -86,4 +96,17 @@ function timeFormat(minute) {
   }
 
   return hour + "h " + min + "min";
+}
+
+function percentage(number, max) {
+  return Math.ceil((number * 100) / max);
+}
+
+function getGenres(data) {
+  var genres = [];
+
+  for (let i=0; i<data.length; i++)
+    genres.push(data[i]['name']);
+
+  return genres;
 }
