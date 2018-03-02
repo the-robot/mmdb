dd8b9418ddbaf2a339383e3772fe4a485186c426
the-robot <the-robot@users.noreply.github.com>
2018-03-02 23:47:01 +0800

    Removed unnecessary whitespaces

--- src/js/pages/MovieCalendar.jsx
+++ src/js/pages/MovieCalendar.jsx
@@ -12,7 +12,6 @@ import MoviesView from '../components/Movie/MoviesView';
     skip: store.calendar.skip,
     fetching: store.calendar.fetching,
   };
-
 })
 export default class MovieCalendar extends React.Component {
   componentWillMount() {
