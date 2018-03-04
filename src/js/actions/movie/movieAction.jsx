c16cc41bab3eedbf83b7f889b8f92f0d9fc3f130
the-robot <the-robot@users.noreply.github.com>
2018-03-05 01:05:08 +0800

    Removed data cleaning methods

--- src/js/actions/movie/movieAction.jsx
+++ src/js/actions/movie/movieAction.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+import { getDate, getGenres, getLanguages, getTime, toPercentage } from '../dataProcess';
 import getTmdbAPIKey from '../../api';
 
 export const fetch = (id) => {
@@ -42,71 +43,10 @@ function clean(data) {
     pg: (data.adult ? 'PG18' : 'PG13'),
     homepage: data.homepage,
     summary: data.overview,
-    rating: percentage(data.vote_average, 10),
+    rating: toPercentage(data.vote_average, 10),
     genres: getGenres(data.genres),
     poster: POSTER_PATH + data.poster_path,
   };
 
   return result;
-}
-
-function getLanguages(languages) {
-  var langs = '';
-
-  // remove empty items
-  for (let lang of languages) {
-    if (lang['name'].length === 0)
-      languages.splice(languages.indexOf(lang), 1);
-  }
-
-  // convert into string
-  for (let lang of languages) {
-    // if last element, do not add comma
-    if (languages.indexOf(lang) === languages.length - 1) {
-      langs += lang['name'];
-      break;
-    }
-
-    langs += lang['name'] + ', ';
-  }
-
-  return langs;
-}
-
-function getDate(date) {
-  const months = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-  ];
-
-  date = date.split("-");
-  
-  var year = date[0];
-  var month = months[parseInt(date[1]) - 1];
-  var day = date[2];
-
-  return month + " " + day + " " + year;
-}
-
-function getTime(minute) {
-  var hour = Math.floor(minute / 60);
-  var min = minute % 60;
-
-  if (hour === 0 && min === 0) {
-    return 'N/A';
-  }
-
-  return hour + "h " + min + "min";
-}
-
-function percentage(number, max) {
-  return Math.ceil((number * 100) / max);
-}
-
-function getGenres(data) {
-  var genres = [];
-
-  for (let i=0; i<data.length; i++)
-    genres.push(data[i]['name']);
-
-  return genres;
 }
